fdc5347450772d96cfd9a1ca524852b75af62346
Patrick Schrangl <pschrangl@beamng.gmbh>
2025-07-11 12:27:58 +0200

    Fix SVN test

--- examples/test_vcs.py
+++ examples/test_vcs.py
@@ -235,6 +235,7 @@ class DummySVNRepo:
             subprocess.check_call(['svn', 'update', os.path.join(self.working_copy_path, 'subdir')])
             subprocess.check_call(['svn', 'delete', os.path.join(self.working_copy_path, 'subdir'), '--force'])
             subprocess.check_call(['svn', 'commit', self.working_copy_path, '-m', 'Fourth commit'])
+            subprocess.check_call(['svn', 'update', self.working_copy_path])
 
         except Exception as e:
             self.cleanup()
